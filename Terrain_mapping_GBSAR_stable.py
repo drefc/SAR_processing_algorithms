8595bc4acb3704d3278d2776062eb5034f32281f
drefc <andre.florentino.c@gmail.com>
2016-05-16 14:58:13 -0500

    Hanning window
    win variable created, to decide whether Hanning window is used or not.

--- Terrain_mapping_GBSAR_stable.py
+++ Terrain_mapping_GBSAR_stable.py
@@ -4,9 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft, ifft
 from scipy import interpolate
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 import time
 import h5py
+
+
+win = 1
+
 
 '''
 RUTAS PARA LA LECTURA DE ARCHIVOS
@@ -43,10 +47,18 @@ for n_toma in range(1,2):
 	nx = dset.attrs['npos']
 	xaf = xai + dax * (nx - 1)
 
+	da = xaf - xai
+	xai = xai - da/2
+	xaf = xaf - da/2
+	
+
 	fre_min = dset.attrs['fi'] * 1E9
 	fre_max = dset.attrs['ff'] * 1E9
+	fre_c = (fre_min+fre_max)/2
+	
 	nfre = dset.attrs['nfre']
 	df = (fre_max - fre_min) / (nfre - 1)
+
 
 	print "Toma numero %d" %n_toma
 	print fre_min
@@ -61,11 +73,18 @@ for n_toma in range(1,2):
 	Dimensiones de la grilla de la reconstruccion, xai y xaf representan
 	las posiciones inicial y final de la antena respectivamente
 	'''
+	'''
+	xi =  xai - 1.0
+	xf =  xaf + 1.0
+	yi =  6.0
+	yf =  10.0
+	'''
+	xi = -12.5
+	xf = 12.5
+	yi =  0.0
+	yf =  25.0
 
-	xi =  xai - 1.5
-	xf =  xaf + 0.5
-	yi =  10.0
-	yf =  15.0
+
 
 	'''
 	Velocidad de la luz, inicialmente el R0 lo hemos considerado 0 para
@@ -73,7 +92,7 @@ for n_toma in range(1,2):
 	'''
 
 	c0  =  299792458.0
-	R0 	=  0.0
+	R0 	=  -1.286
 
 	'''
 	Tamano de los pixeles que buscamos reconstruir:
@@ -81,25 +100,18 @@ for n_toma in range(1,2):
 	 Ambas de 0.01 m, muchisimo mas finas que las resoluciones teoricas.
 	'''
 
-	dx    =  0.01
-	dy    =  0.01
+	dx    =  0.05
+	dy    =  0.05
 	nposx =  int(np.ceil((xf - xi) / dx) + 1)
 	nposy =  int(np.ceil((yf - yi) / dy) + 1)
 	xf    =  xi + dx * (nposx - 1)
 	yf    =  yi + dy * (nposy - 1)
 	
 	
-	#fact  =  10
-	#nr 	  =  nfre * fact
-	##nr =  2 ** np.ceil(np.log2(nfre))
-	
-	dr0  = c0 / (2*df*nfre)
-	fact = dr0 / min(dx,dy)
-	if fact<1:
-		fact = 1
-	
+	fact  =  8
 	#nr =  nfre * fact
 	nr = 2 ** int(np.ceil(np.log2(nfre*fact)))
+	
 	n  = np.arange(nr)
 	B  = df*nr
 	dr = c0 / (2*B)
@@ -131,6 +143,9 @@ for n_toma in range(1,2):
 	yn = yi + dy*np.arange(nposy)
 	Imagen = np.zeros([nposx * nposy], dtype = complex)
 	Rnk = np.zeros([nposx * nposy], dtype = float)
+	
+	s21_arr = np.zeros([nr], dtype = complex)
+
 
 	'''
 	-Primero se calculan las distancias Rnk.
@@ -142,41 +157,68 @@ for n_toma in range(1,2):
 	-Finalmente, se va formando la imagen, primero como un vector, interpolando
 	los valores de Fn obtenidos para que se ajusten a las distancias Rnk de la grilla
 	'''
+	
+	if win==1:
+		Wr = 1.0 - np.cos(2*np.pi*np.arange(1,nfre+1)/(nfre+1))
+		for k in range(0,nx):
+			s21[k,:] = Wr * s21[k,:]
+	
+		Wx = 1.0 - np.cos(2*np.pi*np.arange(1,nx+1)/(nx+1))
+		for f in range(0,nfre):
+			s21[:,f] = Wx * s21[:,f]
+
+	
+	nc0 = int(nfre/2.0)
+	nc1 = int((nfre+1)/2.0)
 
 	for k in range(0,nx):
 		for y in range(0,nposy):
 			for x in range(0, nposx):
 				Rnk[x + y * nposx] = np.sqrt((xn[x]-xa[k])**2+(yn[y])**2)
+			
+		s21_arr[0:nc1] = s21[k,nc0:nfre]
+		s21_arr[nr-nc0:nr] = s21[k,0:nc0]
+
+		Fn0 = nr*ifft(s21_arr, n=nr)
 		
-		Fn0 = ifft(s21[k,:], n = nr)
-		Fn = np.interp(Rnk - R0, rn, np.real(Fn0), period=R) + 1j * np.interp(Rnk - R0, rn, np.imag(Fn0), period=R)
+		Fn = np.interp(Rnk + R0, rn, np.real(Fn0), period=R) + 1j * np.interp(Rnk + R0, rn, np.imag(Fn0), period=R)
 		#fn_re = interpolate.interp1d(rn, np.real(Fn0),kind='cubic')
 		#fn_im = interpolate.interp1d(rn, np.imag(Fn0),kind='cubic')
 		#Fn = fn_re(Rnk - R0) + 1j * fn_im(Rnk - R0)
-		Fn = np.exp(4j * np.pi * (fre_min/c0) * (Rnk - R0)) * Fn 
+
+		Fn = np.exp(4j * np.pi * (fre_c/c0) * (Rnk + R0)) * Fn 
 		
-		Imagen += Rnk**2 * Fn
+		#Imagen += Rnk**2 * Fn
+		Imagen +=  Fn
 
 	'''
 	Se hace un reshape de la Imagen para que corresponda a la forma de la grilla
 	y se normaliza
 	'''
 
-	Imagen /= (nfre * (nx))
+	Imagen /= (nfre * nx)
 	Imagen = np.reshape(Imagen, (nposy, nposx))
 	Imagen = np.flipud(Imagen)
 	Imagen = np.absolute(Imagen)
 	#Imagen *= (1.0/Imagen.max())
-	#Imagen = 20 * np.log(Imagen)
+	ImagendB = 20 * np.log10(Imagen)
+
+	print 'Plotting...'
+
 
 	fig = plt.figure(1)
-	im = plt.imshow(Imagen, cmap = 'jet', aspect = 'auto', extent = [xi,xf,yi,yf], interpolation = 'none')
+#	im = plt.imshow(Imagen, cmap = 'jet', aspect = 'auto', extent = [xi,xf,yi,yf], interpolation = 'none')
+#	im = plt.imshow(ImagendB, cmap = 'jet', aspect = 'auto', extent = [xi,xf,yi,yf], interpolation = 'none')
+	im = plt.imshow(ImagendB, cmap = 'jet', aspect = 'auto', extent = [xi,xf,yi,yf], interpolation = None)
+	im.set_clim(-125,-90)
 	cbar = plt.colorbar(im, orientation = 'vertical')
 	plt.ylabel('Range (m)', fontsize = 14)
 	plt.xlabel('Cross-range (m)', fontsize = 14)
-	plt.show()
 
-	#plt.savefig(RUTA %FECHA_HORA + 'Imagen_%d.png' %n_toma)
+	plt.savefig('Imagen_%d.png' %n_toma)
+
+	plt.show()
+	
 '''
 	Imagen /= (nfre * (nx))
 	Imagen = np.reshape(Imagen, (nposy, nposx))
